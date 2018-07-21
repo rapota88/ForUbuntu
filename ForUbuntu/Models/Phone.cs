00066e3979ffba59f7efdd04c49cb3295e64dfd8
Artem Rapota <artem.rapota@inveritasoft.com>
2018-07-21 22:27:25 +0300

    ForUbuntu

---
+++ ForUbuntu/Models/Phone.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForUbuntu.Models
+{
+    public partial class Phone
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ForUbuntu/Models/postgresContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ForUbuntu.Models
+{
+    public partial class postgresContext : DbContext
+    {
+        public postgresContext()
+        {
+        }
+
+        public postgresContext(DbContextOptions<postgresContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Phone> Phone { get; set; }
+
+        // Unable to generate entity type for table 'public.People'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=admin");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("adminpack");
+
+            modelBuilder.Entity<Phone>(entity =>
+            {
+                entity.ToTable("phone");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasColumnName("name");
+            });
+        }
+    }
+}
